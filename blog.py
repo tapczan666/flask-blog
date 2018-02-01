0a7f426daeb66bf223ef89e0df002be669094239
Maciek W <Maciej.Wojcik4@networks.pl>
2018-02-01 13:38:26 +0100

    Add log in and log out functionality

--- blog.py
+++ blog.py
@@ -6,6 +6,9 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 
 # configuration
 DATABASE = "blog.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = ".f\x96\x01\x9d\x94\xf0S\xd0\xfa\xdeZ\xcb\x15L\x07\xdf\x18\xf9r\xd14\x0e"
 
 app = Flask(__name__)
 
@@ -16,9 +19,25 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def login():
-	return render_template("login.html")
+	error = None
+	status_code = 200
+	if request.method == "POST":
+		if request.form["username"] != app.config["USERNAME"] or request.form["password"] != app.config["PASSWORD"]:
+			error = "Invalid credentials. Please try again."
+			status_code = 401
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+
+	return render_template("login.html", error=error), status_code
+
+@app.route("/logout")
+def logout():
+	session.pop("logged_in", None)
+	flash("You were logged out")
+	return redirect(url_for("login"))
 
 @app.route("/main")
 def main():
