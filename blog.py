c8e269e0819bd7a7fa7bc64e98cf5776d1805271
Maciek W <Maciej.Wojcik4@networks.pl>
2018-02-09 12:09:31 +0100

    Fix some linting errors

--- blog.py
+++ blog.py
@@ -2,7 +2,8 @@
 
 # imports
 import sqlite3
-from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+from flask import Flask, render_template, request, session, flash, redirect, \
+    url_for, g
 
 # configuration
 DATABASE = "blog.db"
@@ -16,32 +17,39 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 # function used for connecting to the database
+
+
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
 
 @app.route('/', methods=["GET", "POST"])
 def login():
-	error = None
-	status_code = 200
-	if request.method == "POST":
-		if request.form["username"] != app.config["USERNAME"] or request.form["password"] != app.config["PASSWORD"]:
-			error = "Invalid credentials. Please try again."
-			status_code = 401
-		else:
-			session['logged_in'] = True
-			return redirect(url_for('main'))
+    error = None
+    status_code = 200
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"] or \
+                request.form["password"] != app.config["PASSWORD"]:
+            error = "Invalid credentials. Please try again."
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
 
-	return render_template("login.html", error=error), status_code
+    return render_template("login.html", error=error), status_code
+
 
 @app.route("/logout")
 def logout():
-	session.pop("logged_in", None)
-	flash("You were logged out")
-	return redirect(url_for("login"))
+    session.pop("logged_in", None)
+    flash("You were logged out")
+    return redirect(url_for("login"))
+
 
 @app.route("/main")
 def main():
-	return render_template('main.html')
+    return render_template('main.html')
+
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug=True)
--- sql.py
+++ sql.py
@@ -2,15 +2,15 @@
 
 import sqlite3
 
-# create a new database 
+# create a new database
 with sqlite3.connect("blog.db") as connection:
-	c = connection.cursor()
+    c = connection.cursor()
 
-	# create a table
-	c.execute("CREATE TABLE posts(title TEXT, post TEXT)")
+    # create a table
+    c.execute("CREATE TABLE posts(title TEXT, post TEXT)")
 
-	# insert some dummy data into the table
-	c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
-	c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
-	c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
-	c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
+    # insert some dummy data into the table
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
