dc00c6949c1951fa61d6e47ff0674514047b27ce
Maciek W <Maciej.Wojcik4@networks.pl>
2018-02-13 09:08:47 +0100

    Add a wrapper to check if the user is logged in

--- blog.py
+++ blog.py
@@ -4,6 +4,7 @@
 import sqlite3
 from flask import Flask, render_template, request, session, flash, redirect, \
     url_for, g
+from functools import wraps
 
 # configuration
 DATABASE = "blog.db"
@@ -21,6 +22,17 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -47,6 +59,7 @@ def logout():
 
 
 @app.route("/main")
+@login_required
 def main():
     return render_template('main.html')
 
